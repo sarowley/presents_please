d26afcae4f5bcb9d960368830d70abdca212c796
sarowley <sarowley@ucsc.edu>
2023-12-08 19:38:06 -0800

    finshed up

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -6,11 +6,21 @@ class Menu extends Phaser.Scene {
   preload() {}
 
   create() {
-    let menuConfig = {
+    let titleConfig = {
       fontSize: "28px",
     };
+    let menuConfig = {
+      fontSize: "20px",
+    };
 
-    this.add.text(200, 200, "click", menuConfig).setOrigin(0.5);
+    //this.add.text(500, 450, "see you space cowboy", menuConfig).setOrigin(0.5);
+    this.add.text(50, 50, "Presents Please", titleConfig);
+    this.add.text(50, 100, "This game is played with the mouse", menuConfig)
+    this.add.text(50, 150, "Click and hold to grab letters as they fall", menuConfig)
+    this.add.text(50, 200, "Drop the letter on the table to open it", menuConfig)
+    this.add.text(50, 250, "Click and drag to throw it to the left or right", menuConfig)
+    this.add.text(50, 300, "Click to begin", menuConfig);
+    this.add.text(200, 450, "Made by Miles Marsh and Sean Rowley", menuConfig);
   }
 
   update() {
--- src/scenes/play.js
+++ src/scenes/play.js
@@ -4,7 +4,6 @@ class Play extends Phaser.Scene {
   }
 
   preload() {
-    
     this.load.image("letter", "./assets/letter.png");
     this.load.image("table", "./assets/table.png");
     this.load.image("letterPile", "./assets/pile_of_letters.png");
@@ -30,39 +29,55 @@ class Play extends Phaser.Scene {
     this.load.image("snowflake", "./assets/snowflake.png");
     this.load.image("coalend", "./assets/coal.png");
     this.load.image("niceending", "./assets/nice_ending.png");
-
-
-
-
-    
   }
 
   create() {
-
     this.karma = 0;
+    this.niceScale = 0.6;
+    this.naughtyScale = 0.5;
+    this.niceScaleCheck = true;
+    this.naughtyScaleCheck = true;
 
+    this.wall = this.add.tileSprite(0, 0, 1000, 1000, "wall").setOrigin(0, 0);
 
-    // this.coalend.depth(10);
-    // this.niceend.depth(10);
+    this.snowfield = this.add
+      .tileSprite(325, 150, 256, 256, "snowfield")
+      .setOrigin(0, 0)
+      .setScale(0.6);
+    this.snow = this.add
+      .tileSprite(325, 150, 256, 256, "snow")
+      .setOrigin(0, 0)
+      .setScale(0.6);
+    this.frame = this.add
+      .tileSprite(325, 150, 256, 256, "frame")
+      .setOrigin(0, 0)
+      .setScale(0.6);
 
+    this.nice = this.add
+      .tileSprite(475, 175, 256, 256, "nice")
+      .setOrigin(0, 0)
+      .setScale(0.6);
+    this.naughty = this.add
+      .tileSprite(25, 175, 512, 256, "naughty")
+      .setOrigin(0, 0)
+      .setScale(0.5);
 
-    this.wall = this.add.tileSprite(0,0,1000,1000, "wall").setOrigin(0,0);
+    this.karmabar = this.add
+      .tileSprite(200, 50, 512, 70, "karmabar")
+      .setOrigin(0, 0)
+      .setScale(0.8);
 
-    this.snowfield = this.add.tileSprite(325, 150, 256, 256, "snowfield").setOrigin(0,0).setScale(0.6);
-    this.snow = this.add.tileSprite(325, 150, 256, 256, 'snow').setOrigin(0, 0).setScale(0.6);
-    this.frame = this.add.tileSprite(325, 150, 256, 256, 'frame').setOrigin(0, 0).setScale(0.6);
-
-    this.nice = this.add.tileSprite(475, 175, 256, 256, 'nice').setOrigin(0, 0).setScale(0.6);
-    this.naughty = this.add.tileSprite(25, 175, 512, 256, 'naughty').setOrigin(0, 0).setScale(0.6);
-
-    this.karmabar = this.add.tileSprite(200, 50, 512,70, 'karmabar').setOrigin(0,0).setScale(0.8);
-
-    this.snowflake = this.add.tileSprite(375, 60,64,64, 'snowflake').setOrigin(0,0).setScale(0.7);
-
+    this.snowflake = this.add
+      .tileSprite(375, 60, 64, 64, "snowflake")
+      .setOrigin(0, 0)
+      .setScale(0.7);
 
     this.matter.add.mouseSpring({ length: 1, stiffness: 1 }); //this is what lets you click on stuff
 
-    this.letter = this.matter.add.image(100, 150, "letter").setBounce(0.9).setScale(0.1);
+    this.letter = this.matter.add
+      .image(100, 150, "letter")
+      .setBounce(0.9)
+      .setScale(0.1);
     this.collideTable = true; //variable to let letter spawn and not be deleted when touching the middle of the table
 
     this.table = this.matter.add
@@ -91,59 +106,61 @@ class Play extends Phaser.Scene {
 
     this.matter.world.on("collisionstart", (event) => {
       //this is the collider
-      this.nice.setVisible(false);
-      this.naughty.setVisible(false);
       if (this.letter.x < 0) {
         //if the letter goes to the left of the screen
         if ((this.number == 2) | (this.number == 3)) {
           //if it's a naughty letter
-          console.log("correct");
-          
           this.snowflake.x += 50;
           this.karma += 1;
-          if(this.karma > 4){
-
-             this.niceend = this.add.tileSprite(0, 0, 512, 512, "niceending").setOrigin(0,0).setScale(1.3,1);
-             this.letter.destroy();
-             return;
+          if (this.karma > 4) {
+            this.niceend = this.add
+              .tileSprite(0, 0, 512, 512, "niceending")
+              .setOrigin(0, 0)
+              .setScale(1.3, 1);
+            this.letter.destroy();
+            return;
           }
         } else {
-          console.log("incorrect");
-
           this.karma -= 1;
           this.snowflake.x -= 50;
-          if(this.karma < -4){
-
-            this.coalend = this.add.tileSprite(0, 0, 512, 512, "coalend").setOrigin(0,0).setScale(1.3);
+          if (this.karma < -4) {
+            this.coalend = this.add
+              .tileSprite(0, 0, 512, 512, "coalend")
+              .setOrigin(0, 0)
+              .setScale(1.3);
             this.letter.destroy();
             return;
           }
         }
         this.number = -1;
         this.letter.destroy();
-        this.letter = this.matter.add.image(100, 150, "letter").setBounce(0.9).setScale(0.1);
+        this.letter = this.matter.add
+          .image(100, 150, "letter")
+          .setBounce(0.9)
+          .setScale(0.1);
       }
       if (this.letter.x > 640) {
         //if the letter goes to the right of the screen
         if ((this.number == 0) | (this.number == 1)) {
           // if it's a nice letter
-          console.log("correct");
           this.snowflake.x += 50;
           this.karma += 1;
-          if(this.karma > 4){
-
-             this.niceend = this.add.tileSprite(0, 0, 512, 512, "niceending").setOrigin(0,0).setScale(1.3, 1);
-             this.letter.destroy();
-             return;
+          if (this.karma > 4) {
+            this.niceend = this.add
+              .tileSprite(0, 0, 512, 512, "niceending")
+              .setOrigin(0, 0)
+              .setScale(1.3, 1);
+            this.letter.destroy();
+            return;
           }
         } else {
-          console.log("incorrect");
           this.karma -= 1;
-
           this.snowflake.x -= 50;
-          if(this.karma < -4){
-
-            this.coalend = this.add.tileSprite(0, 0, 512, 512, "coalend").setOrigin(0,0).setScale(1.3);
+          if (this.karma < -4) {
+            this.coalend = this.add
+              .tileSprite(0, 0, 512, 512, "coalend")
+              .setOrigin(0, 0)
+              .setScale(1.3);
             this.letter.destroy();
             return;
           }
@@ -151,13 +168,19 @@ class Play extends Phaser.Scene {
         this.number = -1;
         this.letter.destroy();
         this.collideTable = true;
-        this.letter = this.matter.add.image(100, 150, "letter").setBounce(0.9).setScale(0.1);
+        this.letter = this.matter.add
+          .image(100, 150, "letter")
+          .setBounce(0.9)
+          .setScale(0.1);
       }
       if (this.letter.x < 150 && this.letter.x > 50) {
         //this is for landing in the letter pile
         this.letter.destroy();
         this.collideTable = true;
-        this.letter = this.matter.add.image(100, 150, "letter").setBounce(0.9).setScale(0.1);
+        this.letter = this.matter.add
+          .image(100, 150, "letter")
+          .setBounce(0.9)
+          .setScale(0.1);
       }
       if (
         this.letter.x < 550 &&
@@ -169,43 +192,72 @@ class Play extends Phaser.Scene {
         this.letter.destroy();
         this.collideTable = false; //lets the letter not be destroyed by the table
         this.number = Math.floor(Math.random() * 4); // this returns 0-3
-        console.log(this.number); //this is the number
         if (this.number == 0) {
           //naughty letter
           this.letter = this.matter.add
             .image(this.currX, this.currY, "letter0")
-            .setBounce(0.9).setScale(0.2);
+            .setBounce(0.9)
+            .setScale(0.2);
         }
         if (this.number == 1) {
           //naughty letter
           this.letter = this.matter.add
             .image(this.currX, this.currY, "letter1")
-            .setBounce(0.9).setScale(0.2);
+            .setBounce(0.9)
+            .setScale(0.2);
         }
         if (this.number == 2) {
           //nice letter
           this.letter = this.matter.add
             .image(this.currX, this.currY, "letter2")
-            .setBounce(0.9).setScale(0.2);
+            .setBounce(0.9)
+            .setScale(0.2);
         }
         if (this.number == 3) {
           //nice letter
           this.letter = this.matter.add
             .image(this.currX, this.currY, "letter3")
-            .setBounce(0.9).setScale(0.2);
+            .setBounce(0.9)
+            .setScale(0.2);
         }
       }
     });
   }
 
   update() {
+    if (this.karma > 4 || this.karma < -4) {
+      this.input.on("pointerdown", () => this.scene.start("menuScene"));
+    }
+
     this.snow.tilePositionX -= 2;
     this.snow.tilePositionY -= 1;
 
-    this.nice.tilePositionX -= 2;
-    this.naughty.tilePositionX += 2;
+    //nice sign pulsing
+    if (this.niceScaleCheck == true) {
+      this.nice.setScale((this.niceScale += 0.002));
+      if (this.niceScale > 0.7) {
+        this.niceScaleCheck = false;
+      }
+    }
+    if (this.niceScaleCheck == false) {
+      this.nice.setScale((this.niceScale -= 0.002));
+      if (this.niceScale < 0.6) {
+        this.niceScaleCheck = true;
+      }
+    }
 
-    // this.wall.tilePositionX -= 10;
-
+    //naughty sign pulsing
+    if (this.naughtyScaleCheck == true) {
+      this.naughty.setScale((this.naughtyScale -= 0.002));
+      if (this.naughtyScale < 0.4) {
+        this.naughtyScaleCheck = false;
+      }
+    }
+    if (this.naughtyScaleCheck == false) {
+      this.naughty.setScale((this.naughtyScale += 0.002));
+      if (this.naughtyScale > 0.5) {
+        this.naughtyScaleCheck = true;
+      }
+    }
   }
 }
